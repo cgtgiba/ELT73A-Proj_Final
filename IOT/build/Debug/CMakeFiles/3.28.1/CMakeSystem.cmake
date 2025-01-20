2bbf2c3aa68cc86f5cf40d25a62b59ca13fa76cc
cgtgiba <cgtgiba@gmail.com>
2025-01-20 19:10:33 -0300

    Acionamento do relé pelo botão e pelo MQTT, OK!!

---
+++ IOT/build/Debug/CMakeFiles/3.28.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.26100")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.26100")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/LAB/ELT73A_Projeto_final/ELT73A-Proj_Final/IOT/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
